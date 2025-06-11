75f94813f1bdee1035fee76908f2c37f7e8f2dba
Vivian Plasencia <v.pcalana@gmail.com>
2025-06-11 10:14:22 +0200

    refactor: update language card component

--- src/components/LanguageCard.tsx
+++ src/components/LanguageCard.tsx
@@ -1,4 +1,4 @@
-import { LuExternalLink } from "react-icons/lu"
+import IconExternalLink from "@theme/Icon/ExternalLink"
 
 export type LanguageCardProps = {
     text: string
@@ -11,11 +11,11 @@ export default function LanguageCard({ text, link }: LanguageCardProps) {
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center space-x-2 border border-[#9fe491] dark:border-[#2d7520] rounded-xl py-6 px-10 dark:bg-[#285d1e] bg-[#caf1c1] dark:hover:bg-[#2d7520] dark:hover:border-[#369524] hover:bg-[#9fe491] hover:border-[#5dca47] hover:!no-underline"
+            className="flex items-center justify-center space-x-2 border border-[#9fe491] dark:border-[#2d7520] rounded-xl py-6 px-10 dark:bg-[#285d1e] bg-[#caf1c1] dark:hover:bg-[#2d7520] dark:hover:border-[#369524] hover:bg-[#9fe491] hover:border-[#5dca47] hover:!no-underline"
         >
             <div className="text-xl font-semibold dark:text-gray-100 text-gray-800">{text}</div>
             <div className="dark:text-gray-100 text-gray-800">
-                <LuExternalLink />
+                <IconExternalLink />
             </div>
         </a>
     )
