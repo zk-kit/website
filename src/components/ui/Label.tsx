e6b33bbb7f04fa6561b0cab529c7ad7ae4f11f16
Kalidou Diagne <kld.diagne@gmail.com>
2025-08-23 03:49:18 +0200

    tailwind safelist for some fonts

--- src/components/ui/Label.tsx
+++ src/components/ui/Label.tsx
@@ -17,10 +17,9 @@ const PageTitle = ({ children, className, as = "h1", size = "lg" }: LabelProps) 
     return createElement(
         as,
         {
-            className: `!font-normal font-clash-grotesk text-app-color-text-base !mb-0 ${className} ${sizeClass[size]}`,
-            style: { 
-                fontFamily: '"ClashGrotesk-Variable", "ClashGrotesk-Medium", system-ui, sans-serif',
-                ...((typeof className === 'string' && className.includes('style')) ? {} : {})
+            className: `!font-normal text-app-color-text-base !mb-0 ${className || ""} ${sizeClass[size]}`,
+            style: {
+                fontFamily: '"ClashGrotesk-Variable", "ClashGrotesk-Medium", system-ui, sans-serif'
             }
         },
         children
@@ -37,10 +36,9 @@ const SectionTitle = ({ children, className, as = "h2", size = "sm" }: LabelProp
     return createElement(
         as,
         {
-            className: `!font-normal font-clash-grotesk text-app-color-text-base !mb-0 ${className} ${sizeClass[size]}`,
-            style: { 
-                fontFamily: '"ClashGrotesk-Variable", "ClashGrotesk-Medium", system-ui, sans-serif',
-                ...((typeof className === 'string' && className.includes('style')) ? {} : {})
+            className: `!font-normal text-app-color-text-base !mb-0 ${className || ""} ${sizeClass[size]}`,
+            style: {
+                fontFamily: '"ClashGrotesk-Variable", "ClashGrotesk-Medium", system-ui, sans-serif'
             }
         },
         children
