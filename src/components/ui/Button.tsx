8d0a9502ff5a041dffe536f783d4a4e91fb27ab9
Vivian Plasencia <v.pcalana@gmail.com>
2025-09-20 00:41:08 +0200

    style: update button style

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -34,7 +34,7 @@ const BUTTON_STYLES = {
 
     variants: {
         primary:
-            "bg-app-color-button-primary-background text-app-color-button-primary-text dark:bg-app-color-button-primary-background dark:text-app-color-button-primary-text hover:bg-app-color-button-primary-background-hover hover:text-app-color-button-primary-text-hover",
+            "bg-app-color-button-primary-background text-app-color-button-primary-text hover:bg-app-color-button-primary-background-hover hover:text-app-color-button-primary-text-hover dark:bg-app-color-button-primary-background dark:text-app-color-button-primary-text",
         secondary:
             "bg-app-color-button-secondary-background text-app-color-button-secondary-text border border-app-color-button-secondary-background hover:bg-app-color-button-secondary-background-hover hover:text-app-color-button-secondary-text-hover"
     } as const,
@@ -53,7 +53,7 @@ const BUTTON_STYLES = {
     } as const,
 
     withShadow: {
-        true: "hover:shadow-lg dark:hover:shadow-[0_0_18px_4px_color-mix(in_srgb,var(--ifm-color-primary-lightest),transparent_70%)] hover:shadow-[0_0_18px_4px_var(--ifm-color-primary-lightest)]",
+        true: "hover:shadow-lg hover:shadow-[0_0_18px_4px_var(--ifm-color-primary-lightest)] dark:hover:shadow-[0_0_18px_4px_color-mix(in_srgb,var(--ifm-color-primary-lightest),transparent_70%)]",
         false: ""
     } as const,
 
